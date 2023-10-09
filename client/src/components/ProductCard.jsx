a973e0e55ab4dfa902453a8be210ccc36f424555
Ramiro Guillen <ramiroguillen@Air.local>
2023-10-09 16:16:43 -0300

    product details view

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   const { id, title, description, category, price, stock, image } = product;
   return (
     <div id={id} className="w-96 h-96 bg-white flex">
-      <img src={image} />
+      <img src={image} alt={title} />
       <div className="flex flex-col p-3 justify-between">
         <h3>{title}</h3>
         <div>
@@ -14,6 +15,9 @@ const ProductCard = ({ product }) => {
           <h5>Available: {stock > 0 ? "Yes" : "No"}</h5>
         </div>
         <div className="flex justify-between gap-1">
+          <Link to={`/${id}`} className="text-sm bg-slate-300">
+            View Product
+          </Link>
           <button className="text-sm bg-slate-300">Add to Wishlist</button>
           <button
             className="text-sm bg-slate-300 disabled:text-slate-200"
--- client/src/views/ProductDetails.jsx
+++ client/src/views/ProductDetails.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useGetProductByIdQuery } from "../features/products/productsApi";
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const { data: product } = useGetProductByIdQuery(id);
-  return <div>ProductDetails</div>;
+  const {
+    data: product,
+    isError,
+    error,
+    isLoading,
+  } = useGetProductByIdQuery(id);
+
+  const [quantity, setQuantity] = useState(1);
+
+  return (
+    <>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : isError ? (
+        <h1>{error}</h1>
+      ) : (
+        product && (
+          <div id={product.id} className="w-1/2 h-96 bg-white flex">
+            <img src={product.image} alt={product.title} />
+            <div className="flex flex-col p-3 justify-between">
+              <h3>{product.title}</h3>
+              <div>
+                <h4 className="mb-3">{product.category}</h4>
+                <p className="text-sm mb-3">{product.description}</p>
+                <h5>Price: ${product.price}</h5>
+                <h5>Available: {product.stock} units</h5>
+                <label>Quantity:</label>
+                <input 
+                className="p-1"
+                  type="number"
+                  min="1"
+                  max={product.stock}
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="flex justify-between gap-1">
+                <button className="text-sm bg-slate-300 p-1">
+                  Add to Wishlist
+                </button>
+                <button className="text-sm bg-slate-300 disabled:text-slate-200 p-1">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        )
+      )}
+    </>
+  );
 };
 
 export default ProductDetails;
